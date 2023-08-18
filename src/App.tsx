a6e974ba907bdd8c780cf13563a7062ccaaf60cb
Your Name <you@example.com>
2023-08-18 23:35:29 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
+//import example from './exemjopa.json'
 import './App.css';
+import { getList } from './api';
+
+const convertList = (data: any): any[] => {
+  return Object.values(data.near_earth_objects).flat().map((x: any) => {
+    return {
+      name: x.name,
+      diameter: Math.floor(x.estimated_diameter.meters.estimated_diameter_max),
+      isDangerous: x.is_potentially_hazardous_asteroid,
+      lunar: x.close_approach_data[0].miss_distance.lunar.split('.')[0],
+      kilometers: x.close_approach_data[0].miss_distance.kilometers.split('.')[0],
+      maxApproachDate: convertDate(x.close_approach_data[0].close_approach_date_full.split(' ')[0])
+    }
+  })
+}
+
+const convertDate = (date: string) => {
+  return date.replace('-Jan-', ' Янв ')
+    .replace('-Feb-', ' Фев ')
+    .replace('-Mar-', ' Март ')
+    .replace('-Apr-', ' Апр ')
+    .replace('-May-', ' Май ')
+    .replace('-Jun-', ' Июнь ')
+    .replace('-Jul-', ' Июль ')
+    .replace('-Aug-', ' Авг ')
+    .replace('-Sep-', ' Сен ')
+    .replace('-Oct-', ' Окт ')
+    .replace('-Nov-', ' Нояб ')
+    .replace('-Dec-', ' Дек ')
+}
+
 
 function App() {
+
+  let [asteroids, setAsteroids] = useState<any[]>([])
+
+  let end = new Date()
+  let start = new Date()
+  end.setDate(end.getDate() - 6)
+
+
+  useEffect(() => {
+    (async () => {
+      let date = await getList(end, start)
+      setAsteroids(convertList(date))
+    })()
+  }, [end, start])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {asteroids.map((x: any) =>
+        <p>{x.diameter}</p>
+      )}
     </div>
   );
 }
---
+++ src/api.tsx
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const apiKey = 'DEMO_KEY'
+const listUrl = 'https://api.nasa.gov/neo/rest/v1/feed'
+
+export async function getList(startDate: Date, endDate: Date,) {
+    
+    const resp = await axios.get(`${listUrl}?start_date=${formatDate(startDate
+        )}&end_date=${formatDate(endDate)}&api_key=${apiKey}`)
+
+    return resp.data
+}
+
+const formatDate = (date: Date) => {
+    let f = `${date.getFullYear()}-${String(date.getMonth()).padStart(2, '0')}-${String(date.getDay()
+        ).padStart(2, '0')}`
+
+    return f
+}
